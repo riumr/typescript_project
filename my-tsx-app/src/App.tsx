b0d4a11c22706e9bd6e002a52c8e46b3b14c3ae5
serviceapperance <108637691+riumr@users.noreply.github.com>
2024-07-08 23:23:32 +0900

    [STYLE] 초기 레이아웃 설정

--- my-tsx-app/src/App.tsx
+++ my-tsx-app/src/App.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const container = {
+    margin:'0 5rem 0 5rem',
+  }
   return (
-    <div className="App">
+    <div style={container}>
+      <div style={{border:'1px solid black', padding:"5rem"}}>
+      </div>
     </div>
   );
 }
